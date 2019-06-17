65d8dcf6867eff51f668e371138dcdb706bf3204
Hoang Quoc Huynh <hoang_quoc_huynh@i-hayabusa.com>
2019-06-17 17:52:11 +0700

    HUynhHQ: Setup env

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import {BirdNestServerApplication} from './application';
 import {ApplicationConfig} from '@loopback/core';
-
+import * as path from 'path';
 export {BirdNestServerApplication};
 
 export async function main(options: ApplicationConfig = {}) {
   const app = new BirdNestServerApplication(options);
   await app.boot();
   await app.start();
-
+  app.static('/assets', path.join(__dirname, 'assets'));
   const url = app.restServer.url;
   console.log(`Server is running at ${url}`);
   console.log(`Try ${url}/ping`);
